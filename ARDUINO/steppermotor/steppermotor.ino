dbc6c32c4760ecbdcd8e7c3b25f9cc065ebf4e9e
PS222407 <ps222407@summacollege.nl>
2021-11-23 19:25:30 +0100

    arduino 6 motor initalization

--- ARDUINO/steppermotor/steppermotor.ino
+++ ARDUINO/steppermotor/steppermotor.ino
@@ -1,65 +1,122 @@
-/*Example sketch to control a stepper motor with A4988/DRV8825 stepper motor driver and Arduino without a library. More info: https://www.makerguides.com */
+int spd = 1500; //MICROSECONDS lager = sneller maar ook minder torque
+
 // Define stepper motor connections and steps per revolution:
-#define dirPin 2
-#define stepPin 3
 #define stepsPerRevolution 200
+
+#define dirPinTOP 2
+#define stepPinTOP 3
+
+#define dirPinFRONT 4
+#define stepPinFRONT 5
+
+#define dirPinBOTTOM 6
+#define stepPinBOTTOM 7
+
+#define dirPinBACK 8
+#define stepPinBACK 9
+
+#define dirPinRIGHT 10
+#define stepPinRIGHT 11
+
+#define dirPinLEFT 12
+#define stepPinLEFT 13
+
 void setup() {
-  // Declare pins as output:
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+  pinMode(stepPinTOP, OUTPUT);    //
+  pinMode(dirPinTOP, OUTPUT);     //
+  digitalWrite(dirPinTOP, HIGH);  //
+
+  pinMode(stepPinFRONT, OUTPUT);
+  pinMode(dirPinFRONT, OUTPUT);
+  digitalWrite(dirPinFRONT, LOW);
+
+  pinMode(stepPinBOTTOM, OUTPUT);
+  pinMode(dirPinBOTTOM, OUTPUT);
+  digitalWrite(dirPinBOTTOM, LOW);
+
+
+
+  pinMode(stepPinBACK, OUTPUT);
+  pinMode(dirPinBACK, OUTPUT);
+  digitalWrite(dirPinBACK, LOW);
+  
+  pinMode(stepPinRIGHT, OUTPUT);
+  pinMode(dirPinRIGHT, OUTPUT);
+  digitalWrite(dirPinRIGHT, LOW);
+  
+  pinMode(stepPinLEFT, OUTPUT);
+  pinMode(dirPinLEFT, OUTPUT);
+  digitalWrite(dirPinLEFT, LOW);
 }
 int count = 0;
 void loop() {
-//  // Set the spinning direction clockwise:
-//  digitalWrite(dirPin, HIGH);
-//  // Spin the stepper motor 1 revolution slowly:
-//  for (int i = 0; i < stepsPerRevolution; i++) {
-//    // These four lines result in 1 step:
-//    digitalWrite(stepPin, HIGH);
-//    delayMicroseconds(2000);
-//    digitalWrite(stepPin, LOW);
-//    delayMicroseconds(2000);
+//  
+//  digitalWrite(dirPinTOP, LOW);
+//  if(count % 2 == 0){
+//    digitalWrite(dirPinTOP, HIGH);
 //  }
-//  delay(1000);
-
-//  // Set the spinning direction counterclockwise:
-//  digitalWrite(dirPin, LOW);
-//  // Spin the stepper motor 1 revolution quickly:
-//  for (int i = 0; i < stepsPerRevolution; i++) {
-//    // These four lines result in 1 step:
-//    digitalWrite(stepPin, HIGH);
-//    delayMicroseconds(1000);
-//    digitalWrite(stepPin, LOW);
-//    delayMicroseconds(1000);
-//  }
-//  delay(1000);
-
-//  // Set the spinning direction clockwise:
-//  digitalWrite(dirPin, HIGH);
-//  // Spin the stepper motor 5 revolutions fast:
-//  for (int i = 0; i < 5 * stepsPerRevolution; i++) {
-//    // These four lines result in 1 step:
-//    digitalWrite(stepPin, HIGH);
-//    delayMicroseconds(500);
-//    digitalWrite(stepPin, LOW);
-//    delayMicroseconds(500);
-//  }
-//  delay(1000);
-
-  // Set the spinning direction counterclockwise:
   
-  digitalWrite(dirPin, LOW);
-  if(count % 2 == 0){
-  digitalWrite(dirPin, HIGH);
-  }
   //Spin the stepper motor 5 revolutions fast:
-  for (int i = 0; i < 50; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(500);
-    count++;
+
+  motorTOP();
+  motorFRONT();
+  motorBOTTOM();
+  
+  motorBACK();
+  motorRIGHT();
+  motorLEFT();
+//  delay(0);
+
+  count++;
+  delay(3000);
+}
+
+
+void motorTOP(){
+    for (int i = 0; i < 150; i++) {
+    digitalWrite(stepPinTOP, HIGH);
+    delayMicroseconds(spd);
+    digitalWrite(stepPinTOP, LOW);
+    delayMicroseconds(spd);
   }
-  delay(4000);
+}
+void motorFRONT(){
+    for (int i = 0; i < 50; i++) {
+    digitalWrite(stepPinFRONT, HIGH);
+    delayMicroseconds(spd);
+    digitalWrite(stepPinFRONT, LOW);
+    delayMicroseconds(spd);
+  }
+}
+void motorBOTTOM(){
+    for (int i = 0; i < 200; i++) {
+    digitalWrite(stepPinBOTTOM, HIGH);
+    delayMicroseconds(spd);
+    digitalWrite(stepPinBOTTOM, LOW);
+    delayMicroseconds(spd);
+  }
+}
+void motorBACK(){
+    for (int i = 0; i < 200; i++) {
+    digitalWrite(stepPinBACK, HIGH);
+    delayMicroseconds(spd);
+    digitalWrite(stepPinBACK, LOW);
+    delayMicroseconds(spd);
+  }
+}
+void motorRIGHT(){
+    for (int i = 0; i < 200; i++) {
+    digitalWrite(stepPinRIGHT, HIGH);
+    delayMicroseconds(spd);
+    digitalWrite(stepPinRIGHT, LOW);
+    delayMicroseconds(spd);
+  }
+}
+void motorLEFT(){
+    for (int i = 0; i < 200; i++) {
+    digitalWrite(stepPinLEFT, HIGH);
+    delayMicroseconds(spd);
+    digitalWrite(stepPinLEFT, LOW);
+    delayMicroseconds(spd);
+  }
 }
